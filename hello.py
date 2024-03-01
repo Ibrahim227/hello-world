f55bb7c24e36da1a49234e473e7f1e0d59f01430
Maman Sani Ibrahim <40499403+Ibrahim227@users.noreply.github.com>
2024-03-01 23:27:48 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ import time #time module
 
 
 name = input("Please enter your name: ")    # Ask the user for his/her name
+last_name = input("Please enter your Last Name:")
+numb = input("enter your number: ")
+email: input("Enter your email:")
+status: input("Enter your status:)
+
 great = f"\tHello {name} ! \n"
 msg = "Welcome to my Github profile !"
 end_msg = "\t Good Bye \n"
@@ -12,11 +17,11 @@ time.sleep(1.5) # wait for 1.5s before displaying the remain message
 
 #multiple print function
 print("#########################################")
-print("# First Name: Maman Sani                #")
-print("# Last Name: Ibrahim                    #")
-print("# Contact : +227 93325219               #")
-print("# Email: mamansani.ibrahim01@gmail.com  #")
-print("# Status : Student | Python Entusiast   #")
+print(f"# First Name: {name}                   #")
+print(f"# Last Name: {last_name}               #")
+print("# Contact :                             #")
+print(f"# Email: {email}                       #")
+print(f"# Status : {status}                     #")
 print("#########################################")
 
 time.sleep(1) # wait for 1s before displaying the end message
